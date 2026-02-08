31dffe58010ca9685acb2aa34a3c0873a46a8432
Malik Danab <51216574+Doomblade712@users.noreply.github.com>
2026-02-08 12:52:12 -0600

    Started GridManagment
    Added
     - GridManager.cs
     - GridCell.cs
     - AddedGridCellPrefab
     - Started on Grid Functionality.

--- FoursightProject/FourSight/Assets/Scripts/GameManager.cs
+++ FoursightProject/FourSight/Assets/Scripts/GameManager.cs
@@ -6,16 +6,68 @@ public class GameManager : MonoBehaviour
 
     private int stage;
 
+    public OptionsManager OptionsManger {  get; private set; }
+    public AudioManager AudioManager { get; private set; }
+    public DeckManager DeckManager { get; private set; }
+
     private void Awake()
     {
         if (instance == null)
         {
             instance = this;
             DontDestroyOnLoad(gameObject);
+            InitializeManagers();
         }
         else if (instance != this)
         {
             Destroy(gameObject);
+        }
+    }
+
+    private void InitializeManagers()
+    {
+        OptionsManger = GetComponentInChildren<OptionsManager>();
+        AudioManager = GetComponentInChildren<AudioManager>();
+        DeckManager = GetComponentInChildren<DeckManager>();
+
+        if(OptionsManger == null)
+        {
+            GameObject prefab = Resources.Load<GameObject>("Prefabs/OptionsManager");
+            if (prefab == null)
+            {
+                Debug.Log($"OptionsManger prefab not found");
+            }
+            else
+            {
+                Instantiate(prefab, transform.position, Quaternion.identity, transform);
+                OptionsManger = GetComponentInChildren<OptionsManager>();
+            }
+        }
+        if (AudioManager == null)
+        {
+            GameObject prefab = Resources.Load<GameObject>("Prefabs/AudioManager");
+            if (prefab == null)
+            {
+                Debug.Log($"AudioManager prefab not found");
+            }
+            else
+            {
+                Instantiate(prefab, transform.position, Quaternion.identity, transform);
+                AudioManager = GetComponentInChildren<AudioManager>();
+            }
+        }
+        if (DeckManager == null)
+        {
+            GameObject prefab = Resources.Load<GameObject>("Prefabs/DeckManager");
+            if (prefab == null)
+            {
+                Debug.Log($"DeckManager prefab not found");
+            }
+            else
+            {
+                Instantiate(prefab, transform.position, Quaternion.identity, transform);
+                DeckManager = GetComponentInChildren<DeckManager>();
+            }
         }
     }
 
